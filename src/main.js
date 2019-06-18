5085b1c85ac97b2ebe1f62f2b928ab3e5fada2a1
Charles Wei <cwei@ultradata.com.au>
2019-06-18 14:23:43 +1000

    init

---
+++ src/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let root = document.getElementById('root');
+let ele = document.createElement('ul');
+root.appendChild(ele);
+
+for (let i = 0; i < 10; i++) {
+    let content = document.createTextNode('Row ' + i);
+    let listItem = document.createElement('li');
+    listItem.appendChild(content);
+    ele.appendChild(listItem);
+}
+
+for (let i = 0; i < 10; i++) {
+    let content = document.createTextNode('New Row ' + i);
+    let listItem = document.createElement('li');
+    listItem.appendChild(content);
+    ele.appendChild(listItem);
+}
+
+let testPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('foo');
+    }, 3000);
+});
+
+testPromise.then(value => {
+    console.log(value);
+})
+
+
